7612a7944b023330d37eb02934308bde7671dae9
Dennis Krul <dennis@tilaa.com>
2015-02-20 12:34:15 +0100

    Log errors to console if we're in cli mode

--- lib/generic/logger.php
+++ lib/generic/logger.php
@@ -2,6 +2,8 @@
 function logger($priority, $message) {
     openlog('abuseio', LOG_ODELAY | LOG_PID | LOG_CONS, LOG_LOCAL1);
     syslog($priority, $message);
+    // Also log to console if we're running in CLI mode
+    if (PHP_SAPI == 'cli') echo $message.PHP_EOL;
     closelog();
 }
 ?>
