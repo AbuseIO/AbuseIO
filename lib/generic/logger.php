6faf9c976dbfb58ab0bdc543755286a6db35ae93
Bart Vrancken <bart@abuse.io>
2015-02-23 19:42:21 +0100

    implement debugging when the argument is given. Else a MTA would get overloaded with logging as thats still a CLI command

--- lib/generic/logger.php
+++ lib/generic/logger.php
@@ -3,7 +3,7 @@ function logger($priority, $message) {
     openlog('abuseio', LOG_ODELAY | LOG_PID | LOG_CONS, LOG_LOCAL1);
     syslog($priority, $message);
     // Also log to console if we're running in CLI mode
-    if (PHP_SAPI == 'cli') echo $message.PHP_EOL;
+    if (PHP_SAPI == 'cli' && DEBUG == true) echo $message.PHP_EOL;
     closelog();
 }
 ?>
