36b867b281b405473f04dd8174ce018c0c58f68f
Bart Vrancken <bart@abuse.io>
2015-02-23 21:18:18 +0100

    complete the notifier

--- lib/core/customers.php
+++ lib/core/customers.php
@@ -105,7 +105,7 @@ function customerLookup($ip) {
     //Lookup failed thus we return dummy
     $customer['Code'] = "UNDEF";
     $customer['Name'] = "Undefined customer";
-    $customer['Contact'] = "undef@local.isp";
+    $customer['Contact'] = "unknown";
     $customer['AutoNotify'] = 0;
 
     return $customer;
