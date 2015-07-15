21166b5f1a26fc6970eb1c402b526934693f359b
Mark Stunnenberg <mark.stunnenberg@tele2.com>
2015-07-15 15:19:25 +0200

    Changed Factory method call to create()

--- app/Commands/EmailProcess.php
+++ app/Commands/EmailProcess.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Filesystem\Filesystem;
 use League\Flysystem\Exception;
 use PhpMimeMailParser\Parser as MimeParser;
-use AbuseIO\Parsers\Factory as GetParser;
+use AbuseIO\Parsers\Factory as ParserFactory;
 use AbuseIO\Commands\EventsValidate;
 use AbuseIO\Commands\EventsSave;
 use Config;
@@ -20,35 +20,35 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 
+    /**
+     * Filename of the email to be processed
+     * @var string
+     */
     public $filename;
 
     /**
-     * Create a new command instance.
-     *
+     * Create a new EmailProcess instance
+     * @param string $filename
      * @return void
      */
     public function __construct($filename)
     {
-
         $this->filename = $filename;
-
     }
 
     /**
      * Queue command into named tube.
-     *
+     * @param  object $queue
+     * @param  string $command
      * @return void
      */
     public function queue($queue, $command)
     {
-
         $queue->pushOn('emails', $command);
-
     }
 
     /**
-     * Execute the command.
-     *
+     * Execute the command
      * @return void
      */
     public function handle()
@@ -64,7 +64,6 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
 
         // Sanity checks
         if (empty($parsedMail->getHeader('from')) || empty($parsedMail->getMessageBody())) {
-
             Log::warning(get_class($this).'Validation failed on: ' . $this->filename);
 
             $this->exception();
@@ -73,14 +72,12 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
 
         // Ignore email from our own notification address to prevent mail loops
         if (preg_match('/' . Config::get('main.notifications.from_address') . '/', $parsedMail->getHeader('from'))) {
-
             Log::warning(
                 get_class($this).'Loop prevention: Ignoring email from self '
                 . Config::get('main.notifications.from_address')
             );
 
             $this->exception();
-
         }
 
         // Start with detecting valid ARF e-mail
@@ -88,88 +85,62 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
         $arfMail = [ ];
 
         foreach ($attachments as $attachment) {
-
             if ($attachment->contentType == 'message/feedback-report') {
-
                 $arfMail['report'] = $attachment->getContent();
-
             }
 
             if ($attachment->contentType == 'message/rfc822') {
-
                 $arfMail['evidence'] = $attachment->getContent();
-
             }
 
             if ($attachment->contentType == 'text/plain') {
-
                 $arfMail['message'] = $attachment->getContent();
-
             }
-
         }
 
         // If we do not have a complete e-mail, then we empty the perhaps partially filled arfMail
         // which is useless, hence reset to false
         if (!isset($arfMail['report']) || !isset($arfMail['evidence']) || !isset($arfMail['message'])) {
-
             $arfMail = false;
-
         }
 
-        // Asking GetParser for an object based on mappings, or die trying
-        $parser = GetParser::object($parsedMail, $arfMail);
+        // Asking ParserFactory for an object based on mappings, or die trying
+        $parser = ParserFactory::create($parsedMail, $arfMail);
 
         $result = false;
         $events = false;
 
         if ($parser !== false) {
-
             $result = $parser->parse();
-
         } else {
-
             Log::error(
                 get_class($this)
                 . ': Unable to handle message from: ' . $parsedMail->getHeader('from')
                 . ' with subject: ' . $parsedMail->getHeader('subject')
             );
             $this->exception();
-
         }
 
         if ($result !== false && $result['errorStatus'] === true) {
-
             Log::error(get_class($parser).': Parser as ended with errors ! : ' . $result['errorMessage']);
-
             $this->exception();
-
         } else {
-
             Log::info(
                 get_class($parser)
                 . ': Parser as ended without errors. Collected ' . count($result['data']) . ' events to save'
             );
-
             $events = $result['data'];
-
         }
 
-        // call validater
-
+        // Call validator
         $validator = new EventsValidate($events);
         $return = $validator->handle();
 
         if ($return['errorStatus'] === true) {
-
             Log::error(get_class($validator).': Validator as ended with errors ! : ' . $return['errorMessage']);
-
             $this->exception();
-
         } else {
-
             Log::info(get_class($validator).': Validator as ended without errors');
-
         }
 
         // save evidence into table
@@ -186,29 +157,24 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
         $return = $saver->handle();
 
         if ($return['errorStatus'] === true) {
-
             Log::error(get_class($saver).': Saver as ended with errors ! : ' . $return['errorMessage']);
-
             $this->exception();
-
         } else {
-
             Log::info(get_class($saver).': Saver as ended without errors');
-
         }
 
     }
 
     /**
-     * We've hit a snag, so we are gracefully killing ourselves after we contact the admin about it.
-     *
-     * @return mixed
+     * We've hit a snag, so we are gracefully killing ourselves
+     * after we contact the admin about it.
+     * @return void
      */
     protected function exception()
     {
         // we have $this->filename and $this->rawMail
         // and this Config::get('main.emailparser.fallback_mail')
-                
+
         Log::error(
             get_class($this).': Ending with errors. The received e-mail will be deleted from '
             . 'archive and bounced to the admin for investigation'
@@ -222,11 +188,8 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
             'emails.bounce',
             '',
             function ($message) {
-
                 $message->from(Config::get('main.notifications.from_address'), 'AbuseIO EmailProcess');
-
                 $message->to(Config::get('main.emailparser.fallback_mail'));
-
                 $message->attach(
                     $this->filename,
                     [
@@ -234,9 +197,7 @@ class EmailProcess extends Command implements SelfHandling, ShouldQueue
                         'mime' => 'message/rfc822',
                     ]
                 );
-
             }
         );
-
     }
 }
