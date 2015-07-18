aad67dcc81e98dd974b96f067b1ce2e7f9db7b9e
Bart Vrancken <bart@abuse.io>
2015-07-18 12:20:04 +0200

    add samples

---
+++ extra/notifier-samples/runall.sh
@@ -0,0 +1,5 @@
+for file in `find /opt/abuseio/extra/notifier-samples/ -type f | grep eml`
+  do
+    echo "pushing $file into queue"
+    cat $file | /usr/bin/php -q /opt/abuseio/artisan email:receive -d
+done
