d38aeac7ac7a04e34d266e053829d107a4880423
Bart Vrancken <bart@abuse.io>
2015-07-25 00:08:11 +0200

    enable debugging with samples

--- extra/notifier-samples/runall.sh
+++ extra/notifier-samples/runall.sh
@@ -1,5 +1,5 @@
 for file in `find /opt/abuseio/extra/notifier-samples/ -type f | grep eml`
   do
     echo "pushing $file into queue"
-    cat $file | /usr/bin/php -q /opt/abuseio/artisan email:receive -d
+    cat $file | /usr/bin/php -q /opt/abuseio/artisan email:receive --debug=true
 done
