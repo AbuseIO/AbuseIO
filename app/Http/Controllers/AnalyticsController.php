4aa34d1fa646cfc70aac6efcdd6e834dec76fc01
Bart Vrancken <bart@abuse.io>
2015-07-25 21:36:59 +0200

    add a very simple overview to validation counts

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -3,6 +3,8 @@
 namespace AbuseIO\Http\Controllers;
 
 use AbuseIO\Http\Requests;
+use AbuseIO\Models\Ticket;
+use Lang;
 
 class AnalyticsController extends Controller
 {
@@ -12,6 +14,25 @@ class AnalyticsController extends Controller
      */
     public function index()
     {
-        return view('analytics');
+        $classCounts = [ ];
+
+        foreach (Lang::get('classifications') as $classID => $classInfo) {
+            $classTotal = new \stdClass();
+
+            $tickets = Ticket::where('class_id', $classID);
+            $ticketCount = $tickets->count();
+            $tickets = $tickets->get();
+
+            if ($ticketCount !== 0) {
+                $classTotal->name = $classInfo['name'];
+                $classTotal->tickets = $tickets->count();
+
+                $classCounts[] = $classTotal;
+            }
+
+        }
+
+        return view('analytics')
+            ->with('classCounts', $classCounts);
     }
 }
--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -2,5 +2,20 @@
 
 @section('content')
     <h1 class="page-header">Analytics</h1>
-    <p></p>
-@stop
+    <table class="table table-striped table-condensed">
+        <thead>
+        <tr>
+            <th>Classification</th>
+            <th>Tickets</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach( $classCounts as $classCount )
+            <tr>
+                <td>{{ $classCount->name }}</td>
+                <td>{{ $classCount->tickets }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
