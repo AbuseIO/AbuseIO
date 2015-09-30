c4a2ce8b412c323be692cec302954dc795f345ef
Bart Vrancken <bart@abuse.io>
2015-09-30 22:53:07 +0200

    fix typo in database name

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'abusio'),
+            'database'  => env('DB_DATABASE', 'abuseio'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
