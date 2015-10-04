2b75e532fb68ea79b94ef4d1adfd602aadaba797
Bart Vrancken <bart@abuse.io>
2015-10-04 19:43:37 +0200

    update comment to use manual timezones

--- app/Providers/MainconfigServiceProvider.php
+++ app/Providers/MainconfigServiceProvider.php
@@ -29,7 +29,9 @@ class MainconfigServiceProvider extends ServiceProvider
         }
 
         /*
-         *
+         * Were updating the timezone manually because we are updating the config later then boot. Using method from:
+         * vendor/laravel/framework/src/Illuminate/Foundation/Bootstrap/LoadConfiguration.php
+         * This enforces the timezone we use in the override config file.
          */
         date_default_timezone_set(config('app.timezone'));
     }
