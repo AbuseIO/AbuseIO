9b0d6098932b5ed0cb30ea232ba74292e478b190
Bart Vrancken <bart@abuse.io>
2015-10-08 20:10:59 +0200

    add default laravel authentication model

---
+++ app/Models/User.php
@@ -0,0 +1,49 @@
+<?php namespace AbuseIO\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+/**
+ * Class Ticket
+ * @package AbuseIO\Models
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ *
+ */
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
+
+    use Authenticatable, CanResetPassword;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table    = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden  = [
+        'password',
+        'remember_token',
+    ];
+}
