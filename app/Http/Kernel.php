b40515e398ed1214e5532a912f358c3e4066e77f
Bart Vrancken <bart@abuse.io>
2015-10-11 12:55:42 +0200

    fix broken language selection after middleware upgrade

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \AbuseIO\Http\Middleware\VerifyCsrfToken::class,
+        \AbuseIO\Http\Middleware\Locale::class,
     ];
 
     /**
