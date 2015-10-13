5b52ec31cdc38e1267621a5bb5ff3e9a9dc6f7ce
Bart Vrancken <bart@abuse.io>
2015-10-13 19:37:25 +0200

    fix psr

--- database/migrations/2015_10_12_154144_create_accounts_table.php
+++ database/migrations/2015_10_12_154144_create_accounts_table.php
@@ -12,11 +12,14 @@ class CreateAccountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('accounts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name')->unique();
-            $table->timestamps();
-        });
+        Schema::create(
+            'accounts',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name')->unique();
+                $table->timestamps();
+            }
+        );
     }
 
     /**
