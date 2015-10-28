1f8ae1fd3031360aec4e595b9ecde1a56055d259
Mark Stunnenberg <mark.stunnenberg@tele2.com>
2015-10-28 17:12:39 +0100

    Fixed a bug where trans_choice wasn't used in the title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>{{ Config::get('app.name') }} {{ Config::get('app.version') }} - {{ trans('misc.'.Request::segment(2)) }}</title>
+	<title>{{ Config::get('app.name') }} {{ Config::get('app.version') }} - {{ trans_choice('misc.'.Request::segment(2), 2) }}</title>
 
 	<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/bootstrap-theme.min.css') }}" rel="stylesheet">
