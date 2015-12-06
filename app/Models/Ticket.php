acb54de2c538d5a4c4094e1bd6717717b33b9584
Bart Vrancken <bart@abuse.io>
2015-12-06 16:08:38 +0100

    readd deleted comments

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -34,8 +34,18 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
 
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
     protected $table    = 'tickets';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'ip',
         'domain',
