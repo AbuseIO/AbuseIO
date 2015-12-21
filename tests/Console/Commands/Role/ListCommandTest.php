244a3ae05284775775c860b81f289d942b62df6f
henkpoldergraaf <m.folkerts@sobit.nl>
2015-12-21 20:46:55 +0100

    added test for role:list command

---
+++ tests/Console/Commands/Role/ListCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace tests\Console\Commands\Role;
+
+use Illuminate\Support\Facades\Artisan;
+use \TestCase;
+
+class ListCommandTest extends TestCase
+{
+    public function testNetBlockListCommand()
+    {
+        $exitCode = Artisan::call('role:list', []);
+
+        $this->assertEquals($exitCode, 0);
+        $this->assertContains("System Administrator", Artisan::output());
+    }
+
+    public function testNetBlockListCommandWithValidFilter()
+    {
+        $exitCode = Artisan::call('role:list', [
+            "--filter" => "Abuse"
+        ]);
+
+        $this->assertEquals($exitCode, 0);
+
+        $output = Artisan::output();
+        $this->assertContains("Abuse", $output);
+        $this->assertNotContains("System Administrator", $output);
+    }
+}
+
