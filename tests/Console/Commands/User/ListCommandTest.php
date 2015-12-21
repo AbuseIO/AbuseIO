d759bf0f7753f315bd910da45e07802574c5efc8
henkpoldergraaf <m.folkerts@sobit.nl>
2015-12-21 21:04:50 +0100

    added user:list tests

---
+++ tests/Console/Commands/User/ListCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace tests\Console\Commands\User;
+
+use Illuminate\Support\Facades\Artisan;
+use \TestCase;
+
+class ListCommandTest extends TestCase
+{
+    public function testNetBlockListCommand()
+    {
+        $exitCode = Artisan::call('user:list', []);
+
+        $this->assertEquals($exitCode, 0);
+        $this->assertContains("admin@isp.local", Artisan::output());
+    }
+
+    public function testNetBlockListCommandWithValidFilter()
+    {
+        $exitCode = Artisan::call('user:list', [
+            "--filter" => "user"
+        ]);
+
+        $this->assertEquals($exitCode, 0);
+
+        $output = Artisan::output();
+        $this->assertContains("user@isp.local", $output);
+        $this->assertNotContains("admin@account2.local", $output);
+    }
+}
+
