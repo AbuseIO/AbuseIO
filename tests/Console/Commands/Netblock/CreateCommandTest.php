c079030c437ef3ec2a90496454ab6811ebc76681
henkpoldergraaf <m.folkerts@sobit.nl>
2016-01-20 11:59:06 +0100

    updated test to reflect new validation messages

--- tests/Console/Commands/Netblock/CreateCommandTest.php
+++ tests/Console/Commands/Netblock/CreateCommandTest.php
@@ -15,11 +15,11 @@ class CreateCommandTest extends TestCase
         $exitCode = Artisan::call(
             'netblock:create',
             [
-                '--contact' => '1',
-                '--first_ip' => '192.168.0.0',
-                '--last_ip' => '192.168.255.255',
-                '--description' => '16-bit block',
-                '--enabled' => 'true',
+                'contact' => '1',
+                'first_ip' => '192.168.0.0',
+                'last_ip' => '192.168.255.255',
+                'description' => '16-bit block',
+                'enabled' => 'true',
             ]
         );
 
@@ -33,7 +33,7 @@ class CreateCommandTest extends TestCase
     {
         $exitCode = Artisan::call('netblock:create');
         $this->assertEquals(0, $exitCode);
-        $this->assertContains("Failed to find contact, could\'nt save netblock", Artisan::output());
+        $this->assertContains('The description field is required.', Artisan::output());
     }
 
     public function testCreateWithoutParamsButValidUser()
@@ -41,13 +41,12 @@ class CreateCommandTest extends TestCase
         $exitCode = Artisan::call(
             'netblock:create',
             [
-                '--contact' => '1',
+                'contact' => '1',
             ]
         );
         $this->assertEquals(0, $exitCode);
-        $this->assertContains(
-            "The first ip must be a valid IP address.\nThe last ip must be a valid IP address.\n".
-            "Failed to create the netblock due to validation warnings\n",
+        $this->assertNotContains(
+            'The contact id field is required',
             Artisan::output()
         );
     }
