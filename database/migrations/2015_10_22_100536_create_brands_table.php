f7625f97a59ddf293d86d4dfad0dced7d015d002
henkpoldergraaf <m.folkerts@sobit.nl>
2016-01-22 09:09:24 +0100

    changed default logo in brand migration to use Brand::getDefaultLogo()

--- database/migrations/2015_10_22_100536_create_brands_table.php
+++ database/migrations/2015_10_22_100536_create_brands_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use AbuseIO\Models\Brand;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
@@ -37,15 +38,13 @@ class CreateBrandsTable extends Migration
         // Always recreate the default branding for the system
         DB::table('brands')->where('id', '=', '1')->delete();
 
-        $abuseio_logo = file_get_contents(base_path('public/images/logo_150.png'));
-
         $brands = [
             [
                 'id'                        => 1,
                 'name'                      => 'default',
                 'company_name'              => 'AbuseIO',
                 'introduction_text'         => 'This is an introduction text',
-                'logo'                      => $abuseio_logo,
+                'logo'                      => Brand::getDefaultLogo(),
                 'account_id'                => 1,
                 'created_at'                => new DateTime,
                 'updated_at'                => new DateTime,
