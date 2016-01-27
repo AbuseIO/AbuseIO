6c8895b108f03c23b085077cefb6a60ae427d223
Mark Stunnenberg <mark@e-rave.nl>
2016-01-27 13:21:03 +0100

    Added foreign key 'exists' validation for contact_id

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 /**
  * Class Domain
  * @package AbuseIO\Models
- * @property integer $id guarded
- * @property string $name
- * @property integer $contact_id
- * @property boolean $enabled
- * @property integer $created_at guarded
- * @property integer $updated_at guarded
- * @property integer $deleted_at guarded
+ * @property integer $id
+ * @property string $name fillable
+ * @property integer $contact_id fillable
+ * @property boolean $enabled fillable
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property integer $deleted_at
  */
 class Domain extends Model
 {
@@ -23,7 +23,7 @@ class Domain extends Model
      *
      * @var string
      */
-    protected $table    = 'domains';
+    protected $table = 'domains';
 
     /**
      * The attributes that are mass assignable.
@@ -33,29 +33,14 @@ class Domain extends Model
     protected $fillable = [
         'name',
         'contact_id',
-        'enabled'
+        'enabled',
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        //
-    ];
-
-    /**
-     * The attributes that cannot be changed
-     *
-     * @var array
-     */
-    protected $guarded  = [
-        'id',
-        'created_at',
-        'updated_at',
-        'deleted_at',
-    ];
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Rules
+    |--------------------------------------------------------------------------
+    */
 
     /**
      * Validation rules for this model being created
@@ -66,7 +51,7 @@ class Domain extends Model
     {
         $rules = [
             'name'          => 'required|unique:domains',
-            'contact_id'    => 'required|integer',
+            'contact_id'    => 'required|integer|exists:contacts,id',
             'enabled'       => 'required|boolean',
         ];
 
@@ -83,31 +68,18 @@ class Domain extends Model
     {
         $rules = [
             'name'          => 'required|unique:domains,name,'. $domain->id,
-            'contact_id'    => 'required|integer',
+            'contact_id'    => 'required|integer|exists:contacts,id',
             'enabled'       => 'required|boolean',
         ];
 
         return $rules;
     }
 
-    /**
-     * Static method to check if the account has access to the model instance
-     *
-     * @param $model_id
-     * @param $account
-     * @return bool
-     */
-    public static function checkAccountAccess($model_id, $account)
-    {
-        // early return when we are in the system account
-        if ($account->isSystemAccount())
-            return true;
-
-        $domain = Domain::find($model_id);
-
-        return ($domain->contact->account->id == $account->id);
-    }
-
+    /*
+    |--------------------------------------------------------------------------
+    | Relationship Methods
+    |--------------------------------------------------------------------------
+    */
 
     /**
      * Returns the contact for this domain
@@ -116,8 +88,31 @@ class Domain extends Model
      */
     public function contact()
     {
-
         return $this->belongsTo('AbuseIO\Models\Contact');
+    }
 
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Methods
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Static method to check if the account has access to the model instance
+     *
+     * @param into                    $model_id
+     * @param \AbuseIO\Models\Account $account
+     * @return bool
+     */
+    public static function checkAccountAccess($model_id, Account $account)
+    {
+        // Early return when we are in the system account
+        if ($account->isSystemAccount()) {
+            return true;
+        }
+
+        $domain = Domain::find($model_id);
+
+        return ($domain->contact->account->id == $account->id);
     }
 }
