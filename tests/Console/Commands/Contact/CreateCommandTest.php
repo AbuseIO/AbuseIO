ba6c757199b04a439aae278e6701f98c786c608b
henkpoldergraaf <m.folkerts@sobit.nl>
2016-01-28 15:29:40 +0100

    added contact:create:command test

--- tests/Console/Commands/Contact/CreateCommandTest.php
+++ tests/Console/Commands/Contact/CreateCommandTest.php
@@ -2,7 +2,9 @@
 
 namespace tests\Console\Commands\Contact;
 
+use AbuseIO\Models\Account;
 use AbuseIO\Models\Contact;
+use Faker\Factory;
 use Illuminate\Support\Facades\Artisan;
 use TestCase;
 
@@ -20,5 +22,28 @@ class CreateCommandTest extends TestCase
         $this->assertContains('The name field is required.', $output);
         $this->assertContains('The account id field is required.', $output);
         $this->assertContains('Failed to create the contact due to validation warnings', $output);
+    }
+
+    public function testValidCreate()
+    {
+        $faker = Factory::create();
+        $name = $faker->name;
+
+
+        Artisan::call('contact:create', [
+            'name' => $name,
+            'reference' => $faker->domainWord,
+            'account_id' => Account::getSystemAccount()->id,
+            'enabled' => $faker->boolean(),
+            'email' => $faker->email,
+            'api_host' => $faker->url,
+        ]);
+
+        $this->assertContains(
+            'The contact has been created',
+            Artisan::output()
+        );
+
+        Contact::where("name", $name)->forceDelete();
     }
 }
