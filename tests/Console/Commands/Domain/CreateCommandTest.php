e70b1d28eef0466271e47ccbccb8aedec74b50fa
henkpoldergraaf <m.folkerts@sobit.nl>
2016-01-28 16:10:27 +0100

    added contact create command test

--- tests/Console/Commands/Domain/CreateCommandTest.php
+++ tests/Console/Commands/Domain/CreateCommandTest.php
@@ -2,6 +2,9 @@
 
 namespace AbuseIO\Console\Commands\Domain;
 
+use AbuseIO\Models\Contact;
+use AbuseIO\Models\Domain;
+use Faker\Factory;
 use Illuminate\Support\Facades\Artisan;
 use TestCase;
 
@@ -18,5 +21,24 @@ class CreateCommandTest extends TestCase
         $this->assertContains('The name field is required.', $output);
         $this->assertContains('The contact id field is required.', $output);
         $this->assertContains('Failed to create the domain due to validation warnings', $output);
+    }
+
+    public function testValidCreate()
+    {
+        $faker = Factory::create();
+
+        $domainName = $faker->domainName;
+
+        Artisan::call('domain:create', [
+            'name' => $domainName,
+            'contact_id' => Contact::all()->first()->id
+        ]);
+
+        $this->assertContains(
+            'The domain has been created',
+            Artisan::output()
+        );
+
+        Domain::where('name', $domainName)->forceDelete();
     }
 }
