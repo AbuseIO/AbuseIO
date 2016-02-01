8c6eaf97ed652bde169c4bb16f383b50c28e3507
henkpoldergraaf <m.folkerts@sobit.nl>
2016-02-01 14:32:44 +0100

    added commented blueprint for incident model factory with test

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -70,19 +70,19 @@ $factory->define(AbuseIO\Models\Event::class, function (Faker\Generator $faker) 
 /**
  * TODO: figure out how to use Incident model factory and what values are relevant.
  */
-$factory->define(AbuseIO\Models\Incident::class, function (Faker\Generator $faker) {
-    return [
-        'source' => 'source',
-        'source_id' => 1,
-        'ip' => $faker->ip,
-        'domain' => $faker->domainName,
-        'uri' => $faker->url,
-        'timestamp' => time(),
-        'class' => 'class',
-        'type' => 'type',
-        'information' => $faker->sentence(60),
-    ];
-});
+//$factory->define(AbuseIO\Models\Incident::class, function (Faker\Generator $faker) {
+//    return [
+//        'source' => 'source',
+//        'source_id' => 1,
+//        'ip' => $faker->ipv4,
+//        'domain' => $faker->domainName,
+//        'uri' => $faker->url,
+//        'timestamp' => time(),
+//        'class' =>  array_rand((trans('classifications'))),
+//        'type' => array_rand(config('types.type')),
+//        'information' => $faker->sentence(60),
+//    ];
+//});
 
 
 
---
+++ tests/Models/IncidentTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\Models;
+
+use AbuseIO\Models\Event;
+use AbuseIO\Models\Evidence;
+use AbuseIO\Models\Incident;
+
+class EventTest extends \TestCase
+{
+    public function testModelFactory()
+    {
+//        $incident = factory(Incident::class)->make();
+//        $this->assertInstanceOf(Incident::class, $incident);
+        // TODO : code is here because can't figure out how to return a data object from model factory.
+        $this->assertTrue(true);
+    }
+}
+
