b44e2973123d16560b43c03edae903a24b89bb1d
Mark Stunnenberg <mark@e-rave.nl>
2016-02-02 13:58:50 +0100

    Fixed a case sensitive typo in calling the Brand model

--- resources/views/brands/create.blade.php
+++ resources/views/brands/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1 class="page-header">{{ trans('brands.headers.new') }}</h1>
-    {!! Form::model(new AbuseIO\Models\brand, ['route' => 'admin.brands.store', 'class' => 'form-horizontal', 'files' => true]) !!}
+    {!! Form::model(new AbuseIO\Models\Brand, ['route' => 'admin.brands.store', 'class' => 'form-horizontal', 'files' => true]) !!}
     @include('brands/partials/_form', ['submit_text' => trans('misc.button.save')])
     {!! Form::close() !!}
 @endsection
