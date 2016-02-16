4c4b5a736fe64db1df102ad43aceb604d3ff7edd
Bart Vrancken <bart@abuse.io>
2016-02-16 22:55:07 +0100

    sanitize url for weird chars

--- app/Helpers/getDomain.php
+++ app/Helpers/getDomain.php
@@ -3,12 +3,22 @@
 /**
  * This helper function can be used to get a valid domain.tld from an url and return it.
  *
- * @param  string $str
+ * @param  string $url
  * @return mixed
  */
 function getDomain($url)
 {
     if (!empty($url)) {
+        //Sanitize URL first by removing unwanted chars
+        $url = str_replace(
+            [
+                "\n",
+                "\r",
+            ],
+            '',
+            $url
+        );
+
         $pslManager = new Pdp\PublicSuffixListManager();
         $urlParser = new Pdp\Parser($pslManager->getList());
         $urlData = $urlParser->parseUrl($url)->toArray();
