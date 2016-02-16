eab3a8d8742db6bb957b1079ceef341653f11de9
Bart Vrancken <bart@abuse.io>
2016-02-16 11:19:10 +0100

    add comment on lost bits

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /*
+         * MARKNL:
+         * OH NOES!! You've lost some bits somewhere! You need at least 64 of them buggers to tun this application.
+         */
         if (PHP_INT_SIZE < 8) {
             Log::emergency(
                 'You will need a 64bit (or higher) PHP/OS to run this application'
