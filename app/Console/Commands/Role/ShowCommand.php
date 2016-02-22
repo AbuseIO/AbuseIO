7941b69949e3e084a3f9fbd496d190ce2423b233
henkpoldergraaf <m.folkerts@sobit.nl>
2016-02-22 14:45:43 +0100

    refactored role showcommand and added tests

--- app/Console/Commands/Role/ShowCommand.php
+++ app/Console/Commands/Role/ShowCommand.php
@@ -2,94 +2,75 @@
 
 namespace AbuseIO\Console\Commands\Role;
 
-use Illuminate\Console\Command;
+
+use AbuseIO\Console\Commands\AbstractShowCommand;
 use AbuseIO\Models\Role;
-use Carbon;
+use Symfony\Component\Console\Input\InputArgument;
 
-class ShowCommand extends Command
+class ShowCommand extends AbstractShowCommand
 {
-
     /**
-     * The console command name.
-     * @var string
+     * {@inherit docs}
      */
-    protected $signature = 'role:show
-                            {--role= : Use the role name or id to show role details }
-    ';
-
-    /**
-     * The console command description.
-     * @var string
-     */
-    protected $description = 'Shows the details of a role';
-
-    /**
-     * The headers of the table
-     * @var array
-     */
-    protected $headers = ['ID', 'Name', 'Description', 'Permissions'];
-
-    /**
-     * The fields of the table / database row
-     * @var array
-     */
-    protected $fields = ['id', 'name', 'description', 'permissions'];
-
-    /**
-     * Create a new command instance.
-     * @return void
-     */
-    public function __construct()
+    protected function getAsNoun()
     {
-        parent::__construct();
+        return "role";
     }
 
     /**
-     * Execute the console command.
-     *
-     * @return boolean
+     * {@inherit docs}
      */
-    public function handle()
+    protected function getAllowedArguments()
     {
-        if (empty($this->option('role'))) {
-            $this->warn('no email or id argument was passed, try --help');
-            return false;
-        }
+        return ["role"];
+    }
 
-        $role = false;
-        if (!is_object($role)) {
-            $role = Role::where('name', $this->option('role'))->first();
-        }
+    /**
+     * {@inherit docs}
+     */
+    protected function getFields()
+    {
+        return [
+            'id',
+            'name',
+            'description',
+        ];
+    }
 
-        if (!is_object($role)) {
-            $role = Role::find($this->option('role'));
-        }
+    /**
+     * {@inherit docs}
+     */
+    protected function getCollectionWithArguments()
+    {
+        return Role::Where("id", $this->argument("role"))->orWhere("name", $this->argument("role"));
+    }
 
-        if (!is_object($role)) {
-            $this->error('Unable to find role with this criteria');
-            return false;
-        }
+    /**
+     * {@inherit docs}
+     */
+    protected function defineInput()
+    {
+        return [
+            new InputArgument(
+                'role',
+                InputArgument::REQUIRED,
+                'Use the id or the name for a role to show it.')
+        ];
+    }
+
+    /**
+     * {@inherit docs}
+     */
+    protected function transformObjectToTableBody($model)
+    {
+        $result = parent::transformObjectToTableBody($model);
 
         $permissions = '';
-        foreach ($role->permissions as $permission) {
+        foreach ($model->permissions as $permission) {
             $permissions .= $permission->name . PHP_EOL;
         }
+        $result[] = ["Permissions", $permissions];
 
-        $table = [ ];
-        $counter = 0;
-        foreach (array_combine($this->headers, $this->fields) as $header => $field) {
-            $counter++;
-            if ($header == 'Permissions') {
-                $table[$counter][] = $header;
-                $table[$counter][] = chop($permissions);
-            } else {
-                $table[$counter][] = $header;
-                $table[$counter][] = (string)$role->$field;
-            }
-        }
-
-        $this->table(['Role Setting', 'Role Value'], $table);
-
-        return true;
+        return $result;
     }
 }
---
+++ tests/Console/Commands/Role/ShowCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace tests\Console\Commands\Role;
+
+use AbuseIO\Models\Role;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
+use TestCase;
+
+/**
+ * Class ShowCommandTest.
+ */
+class ShowCommandTest extends TestCase
+{
+    use DatabaseTransactions;
+
+
+    public function testWithInvalidFilter()
+    {
+        $exitCode = Artisan::call(
+            'role:show',
+            [
+                'role' => 'xxx',
+            ]
+        );
+
+        $this->assertEquals($exitCode, 0);
+        $this->assertContains('No matching role was found.', Artisan::output());
+    }
+
+    public function testWithValidNameFilter()
+    {
+        $exitCode = Artisan::call(
+            'role:show',
+            [
+                'role' => 'Admin',
+            ]
+        );
+
+        $this->assertEquals($exitCode, 0);
+        $this->assertContains('Admin', Artisan::output());
+    }
+}
+
