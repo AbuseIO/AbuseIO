3ef59aa1ebfd687fd37dcb83b2f396c0a60120c7
henkpoldergraaf <m.folkerts@sobit.nl>
2016-02-22 17:26:39 +0100

    refactored show permission command and added tests" "

--- app/Console/Commands/Permission/ShowCommand.php
+++ app/Console/Commands/Permission/ShowCommand.php
@@ -2,94 +2,176 @@
 
 namespace AbuseIO\Console\Commands\Permission;
 
-use Illuminate\Console\Command;
+use AbuseIO\Console\Commands\AbstractShowCommand;
 use AbuseIO\Models\Permission;
-use Carbon;
+use Symfony\Component\Console\Input\InputArgument;
 
-class ShowCommand extends Command
+class ShowCommand extends AbstractShowCommand
 {
-
     /**
-     * The console command name.
-     * @var string
+     * {@inherit docs}
      */
-    protected $signature = 'permission:show
-                            {--permission= : Use the permission name or id to show permission details }
-    ';
-
-    /**
-     * The console command description.
-     * @var string
-     */
-    protected $description = 'Shows the details of a permission';
-
-    /**
-     * The headers of the table
-     * @var array
-     */
-    protected $headers = ['ID', 'Name', 'Description', 'Roles'];
-
-    /**
-     * The fields of the table / database row
-     * @var array
-     */
-    protected $fields = ['id', 'name', 'description', 'permissions'];
-    
-    /**
-     * Create a new command instance.
-     * @return void
-     */
-    public function __construct()
+    protected function getAsNoun()
     {
-        parent::__construct();
+        return "permission";
     }
 
     /**
-     * Execute the console command.
-     *
-     * @return boolean
+     * {@inherit docs}
      */
-    public function handle()
+    protected function getAllowedArguments()
     {
-        if (empty($this->option('permission'))) {
-            $this->warn('no email or id argument was passed, try --help');
-            return false;
-        }
+        return ["id"];
+    }
 
-        $permission = false;
-        if (!is_object($permission)) {
-            $permission = Permission::where('name', $this->option('permission'))->first();
-        }
+    /**
+     * {@inherit docs}
+     */
+    protected function getFields()
+    {
+        return [
+            'id',
+            'name',
+            'description',
+        ];
+    }
 
-        if (!is_object($permission)) {
-            $permission = Permission::find($this->option('permission'));
-        }
+    /**
+     * {@inherit docs}
+     */
+    protected function getCollectionWithArguments()
+    {
+        return Permission::Where("id", $this->argument("permission"));
+    }
 
-        if (!is_object($permission)) {
-            $this->error('Unable to find permission with this criteria');
-            return false;
-        }
+    /**
+     * {@inherit docs}
+     */
+    protected function defineInput()
+    {
+        return [
+            new InputArgument(
+                'permission',
+                InputArgument::REQUIRED,
+                'Use the id for a permission to show it.')
+        ];
+    }
+    /**
+     * {@inherit docs}
+     */
+    protected function transformObjectToTableBody($model)
+    {
+        $result = parent::transformObjectToTableBody($model);
 
-        $roles = '';
-        foreach ($permission->roles as $role) {
-            $roles .= $role->name . PHP_EOL;
+        $roles = [];
+        foreach ($model->roles as $role) {
+            $roles[] = $role->name;
         }
-
-        $table = [ ];
-        $counter = 0;
-        foreach (array_combine($this->headers, $this->fields) as $header => $field) {
-            $counter++;
-            if ($header == 'Roles') {
-                $table[$counter][] = $header;
-                $table[$counter][] = chop($roles);
-            } else {
-                $table[$counter][] = $header;
-                $table[$counter][] = (string)$permission->$field;
+        if ($roles) {
+            $rolesCaption = 'Roles';
+            if (count($roles) === 1) {
+                $rolesCaption = 'Role';
             }
+
+            $result[] = [$rolesCaption, implode(PHP_EOL, $roles)];
         }
 
-        $this->table(['Permission Setting', 'Permission Value'], $table);
-
-        return true;
+        return $result;
     }
 }
+
+
+
+//use Illuminate\Console\Command;
+//use AbuseIO\Models\Permission;
+//use Carbon;
+
+
+
+//class ShowCommand extends Command
+//{
+//
+//    /**
+//     * The console command name.
+//     * @var string
+//     */
+//    protected $signature = 'permission:show
+//                            {--permission= : Use the permission name or id to show permission details }
+//    ';
+//
+//    /**
+//     * The console command description.
+//     * @var string
+//     */
+//    protected $description = 'Shows the details of a permission';
+//
+//    /**
+//     * The headers of the table
+//     * @var array
+//     */
+//    protected $headers = ['ID', 'Name', 'Description', 'Roles'];
+//
+//    /**
+//     * The fields of the table / database row
+//     * @var array
+//     */
+//    protected $fields = ['id', 'name', 'description', 'permissions'];
+//
+//    /**
+//     * Create a new command instance.
+//     * @return void
+//     */
+//    public function __construct()
+//    {
+//        parent::__construct();
+//    }
+//
+//    /**
+//     * Execute the console command.
+//     *
+//     * @return boolean
+//     */
+//    public function handle()
+//    {
+//        if (empty($this->option('permission'))) {
+//            $this->warn('no email or id argument was passed, try --help');
+//            return false;
+//        }
+//
+//        $permission = false;
+//        if (!is_object($permission)) {
+//            $permission = Permission::where('name', $this->option('permission'))->first();
+//        }
+//
+//        if (!is_object($permission)) {
+//            $permission = Permission::find($this->option('permission'));
+//        }
+//
+//        if (!is_object($permission)) {
+//            $this->error('Unable to find permission with this criteria');
+//            return false;
+//        }
+//
+//        $roles = '';
+//        foreach ($permission->roles as $role) {
+//            $roles .= $role->name . PHP_EOL;
+//        }
+//
+//        $table = [ ];
+//        $counter = 0;
+//        foreach (array_combine($this->headers, $this->fields) as $header => $field) {
+//            $counter++;
+//            if ($header == 'Roles') {
+//                $table[$counter][] = $header;
+//                $table[$counter][] = chop($roles);
+//            } else {
+//                $table[$counter][] = $header;
+//                $table[$counter][] = (string)$permission->$field;
+//            }
+//        }
+//
+//        $this->table(['Permission Setting', 'Permission Value'], $table);
+//
+//        return true;
+//    }
+//}
---
+++ tests/Console/Commands/Permission/ShowCommandTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace tests\Console\Commands\Permission;
+
+use AbuseIO\Models\Permission;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Artisan;
+use TestCase;
+
+/**
+ * Class ShowCommandTest.
+ */
+class ShowCommandTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * The list of testing fixtures to test against.
+     *
+     * @var Illuminate\Database\Eloquent\Collection
+     */
+    private $list;
+
+    public function initDB()
+    {
+        $this->list = factory(Permission::class, 10)->create();
+    }
+
+    public function testWithValidIdFilter()
+    {
+        $this->initDB();
+
+        $exitCode = Artisan::call(
+            'permission:show',
+            [
+                'permission' => $this->list->get(0)->id,
+            ]
+        );
+        $this->assertEquals($exitCode, 0);
+        $output = Artisan::output();
+        foreach (['Id',  'Name', 'Description'] as $el) {
+            $this->assertContains($el, $output);
+        }
+    }
+
+    public function testWithInvalidFilter()
+    {
+        $exitCode = Artisan::call(
+            'permission:show',
+            [
+                'permission' => 'xxx',
+            ]
+        );
+
+        $this->assertEquals($exitCode, 0);
+        $this->assertContains('No matching permission was found.', Artisan::output());
+    }
+}
