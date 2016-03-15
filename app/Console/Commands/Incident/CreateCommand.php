3cbca21cc5852d5114ffb335a758f6d6f7a28c5f
Bart Vrancken <bart@abuse.io>
2016-03-15 12:24:01 +0100

    simplify create by just using model, just needs a custom handle() method

--- app/Console/Commands/Incident/CreateCommand.php
+++ app/Console/Commands/Incident/CreateCommand.php
@@ -8,6 +8,7 @@ use Prophecy\Argument;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
 use Validator;
+use AbuseIO\Jobs\EvidenceSave;
 
 /**
  * Class CreateCommand
@@ -22,14 +23,14 @@ class CreateCommand extends AbstractCreateCommand
     {
         return new InputDefinition(
             [
-                new InputArgument('source', InputArgument::REQUIRED, 'source'),
-                new InputArgument("ip", InputArgument::REQUIRED, "ip"),
-                new InputArgument("domain", InputArgument::REQUIRED, "domain"),
-                new InputArgument('uri', InputArgument::REQUIRED, 'uri'),
-                new InputArgument("class", InputArgument::REQUIRED, "class"),
-                new InputArgument("type", InputArgument::REQUIRED, "type"),
-                new InputArgument("timestamp", InputArgument::REQUIRED, "timestamp"),
-                new InputArgument("information", InputArgument::REQUIRED, "information"),
+                new InputArgument('source', InputArgument::REQUIRED, 'Name of the source'),
+                new InputArgument("ip", InputArgument::REQUIRED, "ip address"),
+                new InputArgument("domain", InputArgument::REQUIRED, "domain name"),
+                new InputArgument('uri', InputArgument::REQUIRED, 'uri or path'),
+                new InputArgument("class", InputArgument::REQUIRED, "a preconfigured abuse classification"),
+                new InputArgument("type", InputArgument::REQUIRED, "a preconfigured abuse type"),
+                new InputArgument("timestamp", InputArgument::REQUIRED, "UNIX timestamp"),
+                new InputArgument("information", InputArgument::REQUIRED, "information data in single string or JSON"),
                 new InputArgument('file', InputArgument::OPTIONAL, 'Optionally add a file as evidence'),
             ]
         );
@@ -50,60 +51,59 @@ class CreateCommand extends AbstractCreateCommand
     {
         $incident = new Incident();
 
-        /*
-         * Source can be freely chosen
-         */
         $incident->source = $this->argument('source');
-
-        /*
-         * Domain can be set to - to set this value to false
-         */
-        if ($this->argument('domain') != '-') {
-            // If set this can be a domain or a URL
-            $incident->domain = $this->argument('domain');
-
-            // URI can be set to - to set this value to false
-            $incident->uri = $this->argument('uri');
-        } else {
-            $incident->domain = false;
-            $incident->uri = false;
-        }
-
-        /*
-         * Ip can be set to - to set use resolving, only works if domain is set
-         */
-        if ($this->argument('ip') == '-' &&
-            $this->argument('domain') != '-'
-        ) {
-
-        } else {
-            $incident->ip = $this->argument('ip');
-        }
-
-        /*
-         * Must be existing classes, perhaps a incident:classes & types as helpers?
-         */
+        $incident->ip = $this->argument('ip');
+        $incident->domain = $this->argument('domain');
+        $incident->uri = $this->argument('uri');
         $incident->class = $this->argument('class');
         $incident->type = $this->argument('type');
-
-        /*
-         * Timestamp can be a unix timestamp or a date formated field
-         */
         $incident->timestamp = $this->argument('timestamp');
 
-        /*
-         * This is going to be a challenge to build :/
-         */
-        $incident->information = $this->argument('information');
+        if (is_object(json_decode($this->argument('information')))) {
+            // JSON object given which we can directly use in the incident
+            $incident->information = $this->argument('information');
+
+        } else {
+            // String given so wrapping it into a data json object
+            $incident->information = json_encode(
+                [
+                    'data' => $this->argument('information'),
+                ]
+            );
+        }
 
         /*
-         * Optional field once set it requires a valid is_file() and its added onto the information blob
-         * by building a Evidence tag. Else the Evidence tag would just be the information field.
+         * Save the evidence as its required to save incidents
          */
-        if ($this->argument('file') === 'NULL') {
-            // Build evidence without file
-        } else {
-            // Build evidence with file
+        $evidence = new EvidenceSave;
+        $evidenceData = [
+            'CreatedBy'     => trim(posix_getpwuid(posix_geteuid())['name']) . ' (CLI)',
+            'receivedOn'    => time(),
+            'submittedData' => $incident->toArray(),
+            'attachments'   => [],
+        ];
+
+        // Add the file to evidence object if it was given
+        if ($this->argument('file') !== null) {
+            // Build evidence with added file
+            if (!is_file($this->argument('file'))) {
+                $this->error('File does not exist: ' . $this->argument('file'));
+                die();
+            }
+
+            $attachment = [
+                'filename' => $this->argument('file'),
+                'size' => filesize($this->argument('file')),
+                'contentType' => mime_content_type($this->argument('file')),
+                'data' => file_get_contents($this->argument('file'))
+            ];
+        }
+
+        $evidenceFile = $evidence->save(json_encode($evidenceData));
+
+        if (!$evidenceFile) {
+            $this->error('Error returned while asking to write evidence file, cannot continue');
+            die();
         }
 
         // Somehow call evidenceProcess(with incident wrapped in array, with evidence build)
@@ -118,4 +118,16 @@ class CreateCommand extends AbstractCreateCommand
     {
         return Validator::make($model->toArray(), Incident::createRules());
     }
+
+    /**
+     * Execute the console command.
+     *
+     * @return bool
+     */
+    /*
+    final public function handle()
+    {
+        $model = $this->getModelFromRequest();
+    }
+    */
 }
