86b267a4a5374078fcb5f3f6643c96cc6e465823
henkpoldergraaf <m.folkerts@sobit.nl>
2016-05-16 12:42:45 +0200

    merged change brandcontroller

--- app/Http/Controllers/BrandsController.php
+++ app/Http/Controllers/BrandsController.php
@@ -390,6 +390,7 @@ class BrandsController extends Controller
         }
 
         if (!$brand->canDelete()) {
+            
             return $this->errorForbidden("Can't Delete an active and/or system brand.");
         }
         $brand->delete();
