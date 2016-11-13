640eb0e20056537ae6c5bb89e68e0e99d7e3c253
henkpoldergraaf <m.folkerts@sobit.nl>
2016-11-13 22:50:20 +0100

    added two testmethods checking the logging of the CheckAccount middleware

--- tests/Http/Middleware/CheckAccountTest.php
+++ tests/Http/Middleware/CheckAccountTest.php
@@ -9,10 +9,13 @@ namespace tests\Http\Middleware;
 
 use AbuseIO\Http\Middleware\CheckAccount;
 use AbuseIO\Models\Account;
+use AbuseIO\Models\Event;
+use AbuseIO\Models\Job;
 use AbuseIO\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use tests\TestCase;
 
 class CheckAccountTest extends TestCase
@@ -67,5 +70,49 @@ class CheckAccountTest extends TestCase
         );
 
         $this->isInstanceOf(RedirectResponse::class, $callback);
+    }
+
+    public function testModelHasNoAccountAccessMethodLog()
+    {
+        $this->actingAs(
+            factory(User::class)->create()
+        );
+
+        $model = new Job();
+
+        $r = new Request(['id' => $model->id]);
+// TODO this should have been the other loghandler
+        Log::shouldReceive('notice')
+            ->once()
+            ->with('CheckAccount Middleware is called, with model_id [] for [], which doesn\'t match the model_id format');
+
+        $this->middleware->handle(
+            $r,
+            function ($request) {
+            },
+            $model
+        );
+    }
+
+    public function testModelIdDoesNotRespond()
+    {
+        $this->actingAs(
+            factory(User::class)->create()
+        );
+
+        $model = new Job();
+
+        $r = new Request(['id' => $model->id]);
+// todo the arguments in the log method are not correct;
+        Log::shouldReceive('notice')
+            ->once()
+            ->with('CheckAccount Middleware is called, with model_id [] for [], which doesn\'t match the model_id format');
+
+        $this->middleware->handle(
+            $r,
+            function($request){},
+            $model
+        );
+
     }
 }
