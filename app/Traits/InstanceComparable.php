daadb33c0b8c751228e70e9b3cd27d7fb9bd27cd
henkpoldergraaf <m.folkerts@sobit.nl>
2016-11-15 15:00:09 +0000

    Applied fixes from StyleCI

--- app/Traits/InstanceComparable.php
+++ app/Traits/InstanceComparable.php
@@ -3,19 +3,19 @@
  * Created by PhpStorm.
  * User: martin
  * Date: 15/11/16
- * Time: 14:29
+ * Time: 14:29.
  */
-
 namespace AbuseIO\Traits;
-
 
 trait InstanceComparable
 {
     /**
      * @param $instance
+     *
      * @return bool
      */
-    public function is($instance) {
+    public function is($instance)
+    {
         return
             get_class($this) === get_class($instance)
             && $this->getKey() === $instance->getKey();
