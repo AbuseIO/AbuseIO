19242ff30c9c5674a7cd696507e6fb71c145222f
henkpoldergraaf <m.folkerts@sobit.nl>
2016-11-17 17:13:45 +0100

    added additional testing for User::mayLogin

--- tests/Models/UserTest.php
+++ tests/Models/UserTest.php
@@ -12,4 +12,42 @@ class UserTest extends TestCase
         $user = factory(User::class)->make(['name' => 'testing name']);
         $this->assertEquals($user->name, 'testing name');
     }
+
+    public function testMayLoginSystemAccount()
+    {
+        $user = factory(User::class)->make();
+        $user->account->systemaccount = true;
+
+        $messages = [];
+
+        $this->assertTrue($user->mayLogin($messages));
+        $this->assertTrue(empty($messages));
+    }
+
+    public function testMayLoginWithDisabledAccount()
+    {
+        $user = factory(User::class)->make();
+        $user->account->systemaccount = false;
+        $user->account->disabled = true;
+        $user->disabled = false;
+
+        $messages = [];
+
+        $this->assertFalse($user->mayLogin($messages));
+        $this->assertContains('The account Default for this login is disabled.', $messages);
+    }
+
+    public function testMayLoginWithDisabledUser()
+    {
+        $user = factory(User::class)->make();
+        $user->account->systemaccount = false;
+        $user->account->disabled = false;
+
+        $user->disabled = true;
+
+        $messages = [];
+
+        $this->assertFalse($user->mayLogin($messages));
+        $this->assertContains('This login is disabled.', $messages);
+    }
 }
