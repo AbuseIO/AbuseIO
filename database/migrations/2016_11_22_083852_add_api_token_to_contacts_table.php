e2853b6861238f8749917a87749b9f37062e1b11
henkpoldergraaf <m.folkerts@sobit.nl>
2016-11-22 09:41:07 +0100

    added migration for api-key contacts

---
+++ database/migrations/2016_11_22_083852_add_api_token_to_contacts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddApiTokenToContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->string('api_token', 32)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropColumn('api_token');
+        });
+    }
+}
