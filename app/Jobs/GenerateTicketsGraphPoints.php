9a0dceb40d64dcc1c6922936ab64243f4e0e10e4
henkpoldergraaf <m.folkerts@sobit.nl>
2016-11-23 20:26:18 +0100

    added some convenience methods

--- app/Jobs/GenerateTicketsGraphPoints.php
+++ app/Jobs/GenerateTicketsGraphPoints.php
@@ -2,8 +2,8 @@
 
 namespace AbuseIO\Jobs;
 
-
 use AbuseIO\Models\TicketGraphPoint;
+use Carbon\Carbon;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Support\Facades\DB;
 
@@ -26,25 +26,62 @@ class GenerateTicketsGraphPoints extends Job implements SelfHandling
      */
     public function handle()
     {
-        $data = collect(DB::table('tickets')
+        $this
+            ->storeNewTicketDataForToday()
+            ->storeTouchedDataForToday();
+    }
+
+    public static function getNewDataPointsForToday()
+    {
+        return self::getDataPointsForDateWithScope(Carbon::now(), 'created_at');
+    }
+
+    public static function getNewDataPointsForYesterday()
+    {
+        return self::getDataPointsForDateWithScope(Carbon::yesterday(), 'created_at');
+    }
+
+    public static function getTouchedDataPointsForToday()
+    {
+        return self::getDataPointsForDateWithScope(Carbon::now(), 'updated_at');
+    }
+
+    public static function getTouchedDataPointsForYesterday()
+    {
+        return self::getDataPointsForDateWithScope(Carbon::yesterday(), 'updated_at');
+    }
+
+    private static function getDataPointsForDateWithScope(Carbon $date, $scope)
+    {
+        return collect(DB::table('tickets')
             ->select(
                 DB::raw('count(*) as cnt, class_id, type_id, status_id, contact_status_id')
             )
-            ->whereDay('created_at', '=', date('d', strtotime('-7 days')))
+            ->whereDay($scope, '=', $date->toDateString())
             ->groupBy(['class_id', 'type_id', 'status_id', 'contact_status_id'])
             ->get()
         );
+    }
 
-        $data->map(function ($data) {
-            (new TicketGraphPoint([
-                        'count'             => $data->cnt,
-                        'class'             => $data->class_id,
-                        'type'              => $data->type_id,
-                        'status'            => $data->status_id,
-                        'contact_status'    => $data->contact_status_id,
-                        'lifecycle'         => 'created',
-                        'day_date'          => \Carbon\Carbon::now(),
-                ]))->save();
-        });
+    private function storeNewTicketDataForToday()
+    {
+        self::getNewDataPointsForToday()
+            ->map(
+                function ($data) {
+                    TicketGraphPoint::createNewWithDataForToday($data);
+                }
+            );
+        return $this;
+    }
+
+    private function storeTouchedDataForToday()
+    {
+        self::getTouchedDataPointsForToday()
+            ->map(
+                function($data) {
+                    TicketGraphPoint::createTouchedDataForToday($data);
+                }
+            );
+        return $this;
     }
 }
--- app/Models/TicketGraphPoint.php
+++ app/Models/TicketGraphPoint.php
@@ -2,6 +2,7 @@
 
 namespace AbuseIO\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketGraphPoint extends Model
@@ -33,4 +34,33 @@ class TicketGraphPoint extends Model
         'count',
         'lifecycle',
     ];
+
+    public static function createNewWithDataForToday($data)
+    {
+        return self::createWithDataDateAndLifecycle($data, Carbon::now, 'created_at');
+    }
+
+    public static function createTouchedDataForToday($data)
+    {
+        return self::createWithDataDateAndLifecycle($data, Carbon::now, 'updated_at');
+    }
+
+
+    private static function createWithDataDateAndLifecycle($data, Carbon $date, $lifecycle)
+    {
+        return self::makeWithDataDateAndLifcycle($data, $date, $lifecycle)->save();
+    }
+
+    private static function makeWithDataDateAndLifcycle($data, Carbon $date, $lifecycle)
+    {
+        return new self([
+            'count'             => $data->cnt,
+            'class'             => $data->class_id,
+            'type'              => $data->type_id,
+            'status'            => $data->status_id,
+            'contact_status'    => $data->contact_status_id,
+            'lifecycle'         => $lifecycle,
+            'day_date'          => $date->toDateString()
+        ]);
+    }
 }
