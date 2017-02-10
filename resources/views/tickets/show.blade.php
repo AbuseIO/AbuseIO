f067303c2eb0dbe3502303e7166653ee0677d203
henkpoldergraaf <m.folkerts@sobit.nl>
2017-02-10 16:45:08 +0100

    fixed typo in translation key

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -276,7 +276,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">{!! trans('misc.send_notifictions') !!}</h4>
+                <h4 class="modal-title" id="myModalLabel">{!! trans('misc.send_notifications') !!}</h4>
             </div>
             <div class="modal-body">
                 <ul>
