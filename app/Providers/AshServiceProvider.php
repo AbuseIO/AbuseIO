de061c245fa3c769827b69fe67c5e553d75ab138
joverthegrey <joost@jover.net>
2017-03-24 13:48:27 +0000

    Apply fixes from StyleCI

--- app/Providers/AshServiceProvider.php
+++ app/Providers/AshServiceProvider.php
@@ -22,10 +22,10 @@ class AshServiceProvider extends ServiceProvider
         Ticket::saving(function ($ticket) {
             $salt = env('APP_KEY');
             if (empty($ticket->ash_token_ip)) {
-              $ticket->ash_token_ip = md5($salt.$ticket->id.$ticket->ip.$ticket->ip_contact_reference);
+                $ticket->ash_token_ip = md5($salt.$ticket->id.$ticket->ip.$ticket->ip_contact_reference);
             }
             if (empty($ticket->ash_token_domain)) {
-              $ticket->ash_token_domain = md5($salt . $ticket->id . $ticket->domain . $ticket->domain_contact_reference);
+                $ticket->ash_token_domain = md5($salt.$ticket->id.$ticket->domain.$ticket->domain_contact_reference);
             }
         });
     }
