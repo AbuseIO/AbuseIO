732a2d00f5ff271bb398028dd7f165c5bb0b7230
Mark Stunnenberg <mark.stunnenberg@tele2.com>
2017-10-02 14:14:37 +0200

    Added font assets, logo and related css/js files

---
+++ public/js/global.js
@@ -0,0 +1,7 @@
+// Setup system message slideUp's
+$("#system_message").fadeTo(5000, 500).slideUp(500, function(){
+    $("#system_message").slideUp(500);
+});
+
+// Initialize Material Design
+$.material.init();
