1b9469e32e0f51085e41cfd5d23e5b90b65f92d3
Mark Stunnenberg <mark.stunnenberg@tele2.com>
2017-10-02 15:07:15 +0200

    Added breadcrumbs

---
+++ app/Providers/BreadcrumbsServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AbuseIO\Providers;
+
+use DaveJamesMiller\Breadcrumbs\ServiceProvider;
+
+/**
+ * Class BreadcrumbsServiceProvider
+ * @package AbuseIO\Providers
+ */
+class BreadcrumbsServiceProvider extends ServiceProvider
+{
+	public function registerBreadcrumbs()
+	{
+		require app_path(). '/Http/Routes/Breadcrumbs.php';
+	}
+}
