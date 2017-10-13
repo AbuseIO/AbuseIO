462dd0d1d5d1654055aeae93701c348ca4e8956a
Marknl <mark@e-rave.nl>
2017-10-13 11:14:05 +0000

    Apply fixes from StyleCI

--- app/Helpers/uctrans.php
+++ app/Helpers/uctrans.php
@@ -2,13 +2,13 @@
 
 /**
  * Uppercase the first character of the translated string
- * Simple wrapper around the trans() function
+ * Simple wrapper around the trans() function.
  *
  * @param  $string
  *
  * @return string
  */
-if (! function_exists('uctrans')) {
+if (!function_exists('uctrans')) {
     function uctrans($string)
     {
         return ucfirst(trans($string));
