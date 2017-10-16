ae8ae5c68c34ee719f67bd813e94355ca336efae
Joost Vermeulen <joost@jover.net>
2017-10-16 12:05:48 +0200

    Added forceRootUrl
    Added forceRootUrl to set the base url always to APP_URL. This will solve some problems with proxies and docker

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+// force the base url to the configured APP_URL
+// this helps when AbuseIO is behind a proxy
+URL::forceRootUrl(Config::get('app.url'));
+
 Route::controllers(
     [
         'auth'     => 'Auth\AuthController',
