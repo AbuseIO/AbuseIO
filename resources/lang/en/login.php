cc05b63a67af5b3cd77e6b21eee849ca8bef78e1
Matt Calvert <matt.calvert@ukfast.co.uk>
2020-09-26 10:58:23 +0100

    Fix typo and make CreateComand wording consistent

--- resources/lang/en/login.php
+++ resources/lang/en/login.php
@@ -6,7 +6,7 @@ return [
     'warning.whoops'               => '<strong>Whoops!</strong> There were some problems with your input.',
     'header.login'                 => 'Login',
     'header.reset_password'        => 'Reset Password',
-    'caption.email_address'        => 'Email addres',
+    'caption.email_address'        => 'Email address',
     'caption.password'             => 'Password',
     'caption.remember_me'          => 'Remember me',
     'link.forgot_your_password'    => 'Forgot Your Password?',
