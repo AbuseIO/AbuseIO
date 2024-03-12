ba0a7daa9651e42281cc9cfa3a96a650f2462ff2
JdeGeit <125923512+JdeGeit@users.noreply.github.com>
2024-03-12 09:27:57 +0100

    Update domains.php
    Fixed NL translation

--- resources/lang/nl/domains.php
+++ resources/lang/nl/domains.php
@@ -7,12 +7,12 @@ return [
     // Headers
     'header.new'    => 'Nieuw Domein',
     'header.edit'   => 'Bewerk Domein',
-    'header.detail' => 'Domein details voor',
+    'header.detail' => 'Domeindetails voor',
 
     // Buttons
     'button.new_domain' => 'Nieuw Domein',
 
     // Miscellaneous
     'no_domains' => 'U heeft nog geen domeinen',
-    'domainname' => 'Domein naam',
+    'domainname' => 'Domeinnaam',
 ];
