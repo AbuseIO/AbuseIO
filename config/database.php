f1e5830008d5030e9a091c0cfe4298a34b66a5a1
Bart Vrancken <bart@abuse.io>
2025-10-24 12:23:36 +0000

    upgrade to 5.0.0 - patched upgrade changes LTR 10->11

--- config/database.php
+++ config/database.php
@@ -55,6 +55,20 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_DATABASE', 'abuseio'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
+        'mariadb' => [
+            'driver'    => 'mariadb',
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'abuseio'),
             'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
