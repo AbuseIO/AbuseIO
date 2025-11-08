075f2576bc7b6be00c62b68060ffe9f142d8d330
Bart Vrancken <bart@abuse.io>
2025-11-08 17:46:47 +0000

    allow self signed to TLS adv

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,13 @@ return [
 	    'timeout'      => null,
 	    'auth_mode'    => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+            'stream'       => [
+                'ssl' => [
+                    'verify_peer'      => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed'=> true,
+                ],
+            ],
         ],
 
         'ses' => [
