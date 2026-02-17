cc5b1d27a71fb427d7d3415c5e531da599199d7b
Jesse de Koe <j.dekoe@nbip.nl>
2026-02-17 15:45:03 +0100

    Added the Permission AssignCommandTest

---
+++ tests/Console/Commands/Permission/AssignCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Console\Commands\Permission;
+
+use AbuseIO\Models\Permission;
+use AbuseIO\Models\Role;
+use Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Group;
+use tests\TestCase;
+
+class AssignCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[group('functional')]
+    public function testPermissionRoleAssignmentShouldFaiWithInvalidOptions(): void
+    {
+        $exitCode = Artisan::call('permission:assign', [
+            '--permission' => 'non-existing-permission',
+            '--role' => 'non-existing-role',
+        ]);
+
+        $this->assertEquals(1, $exitCode);
+    }
+
+    #[group('functional')]
+    public function testPermissionRoleAssignmentShouldFailWithMissingOptions(): void
+    {
+        $exitCode = Artisan::call('permission:assign', [
+            // missing options
+        ]);
+
+        $this->assertEquals(1, $exitCode);
+    }
+
+    #[group('functional')]
+    public function testRoleAssignmentShouldPassWithValidIds(): void
+    {
+        $role = Role::create([
+            'name' => 'test-role',
+            'description' => 'A role for testing purposes',
+        ]);
+        $permission = Permission::create([
+            'name' => 'test-permission',
+            'description' => 'A permission for testing purposes',
+        ]);
+
+        $exitCode = Artisan::call('permission:assign', [
+            '--permission' => $permission->id,
+            '--role' => $role->id,
+        ]);
+
+        $this->assertEquals(0, $exitCode);
+    }
+}
